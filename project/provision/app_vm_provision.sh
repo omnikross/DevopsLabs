75f4e3b0b837a1ec589de47c294c61a5f9713446
Omnikross <bms1200@gmail.com>
2020-01-31 17:35:03 +0200

    project

--- project/provision/app_vm_provision.sh
+++ project/provision/app_vm_provision.sh
@@ -1,12 +1,14 @@
+sudo su
 [ ! -d /opt/atlassian/confluence7_1_0/ ] && /vagrant/distro/atlassian-confluence-7.1.0-x64.bin -q -varfile /vagrant/provision/response.varfile
 cp /vagrant/distro/server.xml /opt/atlassian/confluence7_1_0/conf/server.xml 
-cp /vagrant/distro/mysql-connector.deb /opt/atlassian/confluence7_1_0/lib
+cp /vagrant/distro/mysql-connector-java-5.1.48-bin.jar /opt/atlassian/confluence7_1_0/lib
  
+ sudo rm /lib/systemd/system/confluence.service
+ #створення файлика,який буде запускати конфлюенс
 touch /lib/systemd/system/confluence.service
 echo "[Unit]
 Description=Confluence
 After=network.target
-
 [Service]
 Type=forking
 User=confluence
@@ -16,13 +18,17 @@ ExecStop=/opt/atlassian/confluence7_1_0/bin/stop-confluence.sh
 TimeoutSec=200
 LimitNOFILE=4096
 LimitNPROC=4096
-
 [Install]
 WantedBy=multi-user.target" >> /lib/systemd/system/confluence.service
-chmod 664 /lib/systemd/system/confluence.service
+#зміна прав на файлик
+chmod 664 /lib/systemd/system/confluence.service && echo ">>Done creating confluence.service file"
 
-#  sudo /etc/init.d/confluence start
+
+#хз
+server="#log4j.appender.confluencelog.File=${catalina.home}/logs/atlassian-confluence.log"; sed -i "/^#$server/ c$server" file.txt
+
 systemctl daemon-reload
+
 systemctl enable confluence
 systemctl start confluence
 systemctl status confluence
