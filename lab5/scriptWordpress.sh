da3d58b552d7029d846cbc0f84e6ce19e015131f
Omnikross <bms1200@gmail.com>
2020-01-31 17:31:53 +0200

    lab 5

--- lab5/scriptWordpress.sh
+++ lab5/scriptWordpress.sh
@@ -1,4 +1,12 @@
+sudo apt-get update
+sudo apt-get -y remove docker docker-engine docker.io
+sudo apt -y install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+usermod -a -G docker vagrant
+
 docker build -t  wordpress .
 docker rm $(docker ps -a -q)
 
-docker run --name wordpress-container -p 80:80 -d wordpress
+docker run --name mylabwordpress-container -p 80:80 -d wordpress
